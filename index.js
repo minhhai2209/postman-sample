d830933dad0df68290d77d84a9a175bbe0c03825
Hai Minh Nguyen <haimnguyen@qasymphony.com>
2016-11-22 11:04:39 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var newman = require('newman'); // require newman in your project
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+global.qasymphony = {
+  environment: {
+    'req1': 'mot',
+    'req2': 'hai',
+    'req3': 'ba'
+  },
+  globals: {
+    'req1': 'mot',
+    'req2': 'hai',
+    'req3': 'ba'
+  }
+};
+
+var stopped = false;
+
+// call newman.run to pass `options` object and wait for callback
+newman.run({
+  collection: require('./sample-collection.json'),
+  reporters: 'cli'
+}, function (err) {
+  if (err) { throw err; }
+  console.log('collection run complete!');
+}).on('beforeTest', function() {
+  if (!stopped) {
+    global.qasymphony.nextRequest = 'ba';
+    stopped = true;
+  }
+});
